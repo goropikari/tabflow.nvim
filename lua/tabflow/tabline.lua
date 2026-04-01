9d992934d81e1b71f5872312da4ff76223f00970
goropikari <goropikari56@gmail.com>
2026-04-01 21:26:58 +0900

    feat: fix mode toggle to left and implement viewport for many buffers

--- lua/tabflow/tabline.lua
+++ lua/tabflow/tabline.lua
@@ -91,49 +91,128 @@ function M.build_items()
 end
 
 function M.render_items(items)
+  local total_width = vim.o.columns
   local res = ''
   local layout_items = {}
   local current_col = 0
 
-  for i, item in ipairs(items) do
+  -- Separate mode toggle from other items
+  local mode_toggle_item = nil
+  local other_items = {}
+  for _, item in ipairs(items) do
+    if item.kind == 'mode_toggle' then
+      mode_toggle_item = item
+    else
+      table.insert(other_items, item)
+    end
+  end
+
+  -- 1. Render Mode Toggle on the left (Fixed)
+  if mode_toggle_item then
     local hl = M.HL.inactive
-    if item.active then
-      hl = M.HL.active
-    end
+    local label = ' ' .. mode_toggle_item.label .. ' '
+    local width = vim.fn.strdisplaywidth(label)
 
-    if state.state.drag.active and state.state.drag.hover_target then
-      if state.state.drag.hover_target.kind == item.kind and state.state.drag.hover_target.id == item.id then
-        hl = M.HL.hover
-      end
-    end
+    res = res .. '%#' .. hl .. '#' .. label
 
-    -- Add icon for buffer items
+    table.insert(layout_items, {
+      kind = mode_toggle_item.kind,
+      label = mode_toggle_item.label,
+      start_col = current_col,
+      end_col = current_col + width,
+      index = mode_toggle_item.index,
+    })
+    current_col = current_col + width
+  end
+
+  -- 2. Calculate widths and find active item for viewport logic
+  local active_idx = 1
+  for i, item in ipairs(other_items) do
     local icon = ''
     if item.kind == 'buffer' and item.id then
-      local icon_char, icon_color = get_icon(item.id)
+      local icon_char = get_icon(item.id)
       if icon_char then
         icon = icon_char .. ' '
       end
     end
-
-    local label = ' ' .. icon .. item.label .. ' '
-    local start_col = current_col
-    local item_len = #label
-    local end_col = start_col + item_len
-
-    res = res .. '%#' .. hl .. '#' .. label
-    current_col = end_col
-
-    table.insert(layout_items, {
-      kind = item.kind,
-      id = item.id,
-      label = item.label,
-      start_col = start_col,
-      end_col = end_col,
-      index = item.index,
-    })
+    item.full_label = ' ' .. icon .. item.label .. ' '
+    item.display_width = vim.fn.strdisplaywidth(item.full_label)
+    if item.active then
+      active_idx = i
+    end
   end
 
+  -- 3. Viewport logic: select a range of items that fits in the remaining space
+  local available_width = total_width - current_col - 4 -- Small margin
+  local start_idx = active_idx
+  local end_idx = active_idx
+  local items_width = (other_items[active_idx] and other_items[active_idx].display_width) or 0
+
+  if #other_items > 0 then
+    while true do
+      local expanded = false
+      -- Try to expand left
+      if start_idx > 1 then
+        local w = other_items[start_idx - 1].display_width
+        if items_width + w + (start_idx > 2 and 3 or 0) <= available_width then
+          start_idx = start_idx - 1
+          items_width = items_width + w
+          expanded = true
+        end
+      end
+      -- Try to expand right
+      if end_idx < #other_items then
+        local w = other_items[end_idx + 1].display_width
+        if items_width + w + (end_idx < #other_items - 1 and 3 or 0) <= available_width then
+          end_idx = end_idx + 1
+          items_width = items_width + w
+          expanded = true
+        end
+      end
+      if not expanded then
+        break
+      end
+    end
+  end
+
+  -- 4. Render selected viewport range
+  if #other_items > 0 then
+    if start_idx > 1 then
+      res = res .. '%#' .. M.HL.fill .. '#.. '
+      current_col = current_col + 3
+    end
+
+    for i = start_idx, end_idx do
+      local item = other_items[i]
+      if item then
+        local hl = item.active and M.HL.active or M.HL.inactive
+
+        if state.state.drag.active and state.state.drag.hover_target then
+          if state.state.drag.hover_target.kind == item.kind and state.state.drag.hover_target.id == item.id then
+            hl = M.HL.hover
+          end
+        end
+
+        res = res .. '%#' .. hl .. '#' .. item.full_label
+
+        table.insert(layout_items, {
+          kind = item.kind,
+          id = item.id,
+          label = item.label,
+          start_col = current_col,
+          end_col = current_col + item.display_width,
+          index = item.index,
+        })
+        current_col = current_col + item.display_width
+      end
+    end
+
+    if end_idx < #other_items then
+      res = res .. '%#' .. M.HL.fill .. '# ..'
+    end
+  end
+
+  -- Fill the rest
   res = res .. '%#' .. M.HL.fill .. '#%='
 
   return res, layout_items
