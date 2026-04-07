e117f8cd041d8dcea8bfff046f8e97fd5d2c4e4e
goropikari <goropikari56@gmail.com>
2026-04-08 01:16:58 +0900

    format

--- lua/tabflow/init.lua
+++ lua/tabflow/init.lua
@@ -32,34 +32,52 @@ function M.setup(opts)
     TabflowPrevTab = { fn = actions.prev_tab, opts = {} },
     TabflowNextBuffer = { fn = actions.next_buffer, opts = {} },
     TabflowPrevBuffer = { fn = actions.prev_buffer, opts = {} },
-    TabflowRenameTab = { fn = function(args)
-      actions.rename_tab(vim.api.nvim_get_current_tabpage(), args.args)
-    end, opts = { nargs = 1 } },
-    TabflowSetGitBranchName = { fn = function()
-      require('tabflow.state').set_tab_name_to_git_branch()
-    end, opts = { nargs = 0 } },
-    TabflowNewTab = { fn = function()
-      vim.cmd('tabnew')
-      actions.enter_buffers_mode()
-    end, opts = {} },
-    TabflowCloseTab = { fn = function()
-      actions.close_tab(vim.api.nvim_get_current_tabpage())
-    end, opts = {} },
-    TabflowCloseBuffer = { fn = function()
-      actions.close_buffer(vim.api.nvim_get_current_buf())
-    end, opts = {} },
-    TabflowDeleteOtherBuffers = { fn = actions.delete_other_buffers, opts = {} },
-    TabflowOpenWorktree = { fn = function(args)
-      actions.select_worktree(args.args)
-    end, opts = {
-      nargs = '?',
-      complete = function()
-        local worktrees = require('tabflow.state').get_git_worktrees()
-        return vim.tbl_map(function(wt)
-          return wt.branch
-        end, worktrees)
+    TabflowRenameTab = {
+      fn = function(args)
+        actions.rename_tab(vim.api.nvim_get_current_tabpage(), args.args)
       end,
-    } },
+      opts = { nargs = 1 },
+    },
+    TabflowSetGitBranchName = {
+      fn = function()
+        require('tabflow.state').set_tab_name_to_git_branch()
+      end,
+      opts = { nargs = 0 },
+    },
+    TabflowNewTab = {
+      fn = function()
+        vim.cmd('tabnew')
+        actions.enter_buffers_mode()
+      end,
+      opts = {},
+    },
+    TabflowCloseTab = {
+      fn = function()
+        actions.close_tab(vim.api.nvim_get_current_tabpage())
+      end,
+      opts = {},
+    },
+    TabflowCloseBuffer = {
+      fn = function()
+        actions.close_buffer(vim.api.nvim_get_current_buf())
+      end,
+      opts = {},
+    },
+    TabflowDeleteOtherBuffers = { fn = actions.delete_other_buffers, opts = {} },
+    TabflowOpenWorktree = {
+      fn = function(args)
+        actions.select_worktree(args.args)
+      end,
+      opts = {
+        nargs = '?',
+        complete = function()
+          local worktrees = require('tabflow.state').get_git_worktrees()
+          return vim.tbl_map(function(wt)
+            return wt.branch
+          end, worktrees)
+        end,
+      },
+    },
   }
 
   if type(opts.commands) == 'table' then
